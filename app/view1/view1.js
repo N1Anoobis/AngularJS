cf498eff4ba5a48f26a9e785735007bb24e73531
Slawomir Kaczynski <slawomir.kaczynski@billennium.com>
2021-07-15 09:47:07 +0200

    Add adding funcionality

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,7 +9,6 @@ angular
       $routeProvider.when("/view1", {
         templateUrl: "view1/view1.html",
         controller: "View1Ctrl",
-        
       });
     },
   ])
@@ -17,41 +16,67 @@ angular
   .controller("View1Ctrl", [
     "$scope",
     function ($scope) {
+      $scope.removeNinja = function (ninja) {
+        const removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removedNinja, 1);
+      };
+
+      $scope.addNinja = function () {
+        $scope.ninjas.push({
+          name: $scope.newNinja.name,
+          belt: $scope.newNinja.belt,
+          rate: parseInt($scope.newNinja.rate), 
+          available: true,
+        });
+        $scope.newNinja.name = "";
+        $scope.newNinja.belt = "";
+        $scope.newNinja.rate = "";
+      };
+
       $scope.ninjas = [
         {
           name: "yoshi",
           belt: "green",
           rate: 50,
+          available: false,
         },
         {
           name: "bond",
           belt: "blue",
           rate: 20,
+          available: true,
         },
         {
           name: "rychu",
           belt: "red",
           rate: 80,
+          available: true,
         },
         {
           name: "artur",
           belt: "black",
           rate: 60,
+          available: true,
         },
       ];
       $scope.content = {
-        "color": "orange",
+        color: "orange",
         "background-color": "gray",
-        "display": "flex",
-        "flex-direction":"column",
+        display: "flex",
+        "flex-direction": "column",
         "align-items": "center",
         "min-height": "97vh",
-    }
+      };
 
-    $scope.input = {
-      "margin": "0 auto",
-      "width": "200px",
-
-  }
+      $scope.input = {
+        margin: "0 auto",
+        width: "200px",
+      };
+      $scope.cross = {
+        color: "red",
+        display: "inline",
+        "padding-left": "15px",
+        cursor: "pointer",
+      };
     },
   ]);
